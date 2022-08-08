a6ac94744d8cac4bd00e30960eae714b8114cdf1
Zachary Baca <bacazach@icloud.com>
2022-08-08 12:34:04 -0500

    created about and dynamic project route and successfully launched app on port 3000

--- app.js
+++ app.js
@@ -14,4 +14,18 @@ app.use('/static', express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.locals = projects.projects;
     res.render('index');
+});
+
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/project/:id', (req, res) => {
+    const projectId = req.params.id;
+    res.render('project', {"project_id": projectId});
+});
+
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000');
 })
